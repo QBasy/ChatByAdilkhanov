35b87d1ec3976f0d07cf4b1e69e0a8ccce30a658
QoidynBasy <c0mplexie1@gmail.com>
2023-06-18 00:00:50 +0600

    Not First commit

--- ClientHandler.java
+++ ClientHandler.java
@@ -7,4 +7,36 @@ public class ClientHandler extends Thread {
     private Socket clientSocket;
     private BufferedReader reader;
     private PrintWriter writer;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            writer = new PrintWriter(clientSocket.getOutputStream());
+            String message;
+
+            while ((message = reader.readLine()) != null) {
+                System.out.println("Received message from client: " + message);
+                writer.println("Message received: " + message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+                if (writer != null) {
+                    writer.close();
+                }
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
